477159b383b7d7e0f73851477496806a0e6a4fe0
kkboranbay <kkboranbay@gmail.com>
2023-11-19 13:08:29 +0600

    added instrumenting middleware for service

--- main.go
+++ main.go
@@ -5,8 +5,12 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/go-kit/kit/metrics"
+	"github.com/go-kit/kit/metrics/prometheus"
 	httptransport "github.com/go-kit/kit/transport/http"
 	"github.com/go-kit/log"
+	stdprometheus "github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
@@ -19,13 +23,34 @@ func main() {
 		// ts=2023-11-19T06:20:31.612571Z caller=middleware.go:32 method=Concat a=leo b=ken v=leoken err=null
 	}
 
+	var ints, chars metrics.Counter
+	{
+		// Business-level metrics.
+		ints = prometheus.NewCounterFrom(stdprometheus.CounterOpts{
+			Namespace: "go_kit",
+			Subsystem: "add_service",
+			Name:      "integers_summed",
+			Help:      "Total count of integers summed via the Sum method.",
+		}, []string{})
+
+		chars = prometheus.NewCounterFrom(stdprometheus.CounterOpts{
+			Namespace: "go_kit",
+			Subsystem: "add_service",
+			Name:      "characters_concatenated",
+			Help:      "Total count of characters concatenated via the Concat method.",
+		}, []string{})
+	}
+
 	svc := service.NewBasicService()
 	svc = service.LoggingMiddleware(logger)(svc)
+	svc = service.InstrumentingMiddleware(ints, chars)(svc)
 
 	sumHandler := httptransport.NewServer(MakeSumEndpoint(svc), decodeHTTPSumRequest, encodeHTTPGenericResponse)
 	concatHandler := httptransport.NewServer(MakeConcatEndpoint(svc), decodeHTTPConcatRequest, encodeHTTPGenericResponse)
 
 	http.Handle("/sum", sumHandler)
 	http.Handle("/concat", concatHandler)
+	// ../prometheus/prometheus --config.file=.config/prometheus.yml
+	http.Handle("/metrics", promhttp.Handler())
 	http.ListenAndServe(":8080", nil)
 }
--- service/middleware.go
+++ service/middleware.go
@@ -3,6 +3,7 @@ package service
 import (
 	"context"
 
+	"github.com/go-kit/kit/metrics"
 	"github.com/go-kit/log"
 )
 
@@ -33,4 +34,31 @@ func (mw loggingMiddleware) Concat(ctx context.Context, a, b string) (v string, 
 	}()
 
 	return mw.next.Concat(ctx, a, b)
+}
+
+// InstrumentingMiddleware returns a service middleware that instruments
+// the number of integers summed and characters concatenated over the lifetime of
+// the service.
+func InstrumentingMiddleware(ints, chars metrics.Counter) Middleware {
+	return func(next Service) Service {
+		return instrumentingMiddleware{ints, chars, next}
+	}
+}
+
+type instrumentingMiddleware struct {
+	ints  metrics.Counter
+	chars metrics.Counter
+	next  Service
+}
+
+func (mw instrumentingMiddleware) Sum(ctx context.Context, a, b int) (int, error) {
+	v, err := mw.next.Sum(ctx, a, b)
+	mw.ints.Add(float64(v))
+	return v, err
+}
+
+func (mw instrumentingMiddleware) Concat(ctx context.Context, a, b string) (string, error) {
+	v, err := mw.next.Concat(ctx, a, b)
+	mw.chars.Add(float64(len(v)))
+	return v, err
 }
